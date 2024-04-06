feb5c2376899a8dfd1b5aa3b1df7b8053184bdb9
kent200111 <s.maximo.rommelkent@cmu.edu.ph>
2024-04-06 17:24:59 +0800

    calendar restriction

--- app/Http/Controllers/FullCalendarController.php
+++ app/Http/Controllers/FullCalendarController.php
@@ -1,30 +1,42 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
-use App\Models\Event; //add Event Model
- 
+use App\Models\Event; // Add Event Model
+use Illuminate\Support\Facades\Auth;
+
 class FullCalendarController extends Controller
 {
-    public function getEvent(){
-        if(request()->ajax()){
+    public function getEvent()
+    {
+        if (request()->ajax()) {
             $start = (!empty($_GET["start"])) ? ($_GET["start"]) : ('');
             $end = (!empty($_GET["end"])) ? ($_GET["end"]) : ('');
-            $events = Event::whereDate('start', '>=', $start)->whereDate('end',   '<=', $end)
-                    ->get(['id','title','start', 'end']);
+            $events = Event::whereDate('start', '>=', $start)->whereDate('end', '<=', $end)
+                ->get(['id', 'title', 'start', 'end']);
             return response()->json($events);
         }
         return view('fullcalendar');
-         
     }
-    public function createEvent(Request $request){
+
+    public function createEvent(Request $request)
+    {
+        if (Auth::user()->usertype !== 'admin') {
+            abort(403, 'Only administrators can create events.');
+        }
+
         $data = $request->except('_token');
         $events = Event::insert($data);
         return response()->json($events);
     }
-    
-    public function deleteEvent(Request $request){
+
+    public function deleteEvent(Request $request)
+    {
+        if (Auth::user()->usertype !== 'admin') {
+            abort(403, 'Only administrators can delete events.');
+        }
+
         $event = Event::find($request->id);
         return $event->delete();
     }
--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>CMU Calendar </title>
+    <title>CMU Calendar</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css' rel='stylesheet'>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>
@@ -27,73 +27,88 @@
 </body>
 
 <style>
-.fc-event {
-    padding: 8px;
-    /* Adjust padding to make events larger */
-    font-size: 16px;
-    /* Adjust font size for event title */
-}
+    .fc-event {
+        padding: 8px;
+        /* Adjust padding to make events larger */
+        font-size: 16px;
+        /* Adjust font size for event title */
+    }
 </style>
 
 <script>
-$(document).ready(function() {
-    var calendar = $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,basicWeek,basicDay'
-        },
-        navLinks: true,
-        editable: true,
-        events: "getevent",
-        displayEventTime: false,
-        eventRender: function(event, element, view) {
-            if (event.allDay === 'true') {
-                event.allDay = true;
-            } else {
-                event.allDay = false;
-            }
-        },
-        selectable: true,
-        selectHelper: true,
-        select: function(start, end, allDay) {
-            var title = prompt('Event Title:');
-            if (title) {
-                var start = moment(start, 'DD.MM.YYYY').format('YYYY-MM-DD');
-                var end = moment(end, 'DD.MM.YYYY').format('YYYY-MM-DD');
-                $.ajax({
-                    url: "{{ URL::to('createevent') }}",
-                    data: 'title=' + title + '&start=' + start + '&end=' + end +
-                        '&_token=' + "{{ csrf_token() }}",
-                    type: "post",
-                    success: function(data) {
-                        alert("Added Successfully");
-                        $('#calendar').fullCalendar('refetchEvents');
-                    }
-                });
-            }
-        },
-        eventClick: function(event) {
-            var deleteMsg = confirm("Do you really want to delete?");
-            if (deleteMsg) {
-                $.ajax({
-                    type: "POST",
-                    url: "{{ URL::to('deleteevent') }}",
-                    data: "&id=" + event.id + '&_token=' + "{{ csrf_token() }}",
-                    success: function(response) {
-                        if (parseInt(response) > 0) {
-                            $('#calendar').fullCalendar('removeEvents', event.id);
-                            alert("Deleted Successfully");
+    $(document).ready(function () {
+        var calendar = $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,basicWeek,basicDay'
+            },
+            navLinks: true,
+            editable: true,
+            events: "getevent",
+            displayEventTime: false,
+            eventRender: function (event, element, view) {
+                if (event.allDay === 'true') {
+                    event.allDay = true;
+                } else {
+                    event.allDay = false;
+                }
+            },
+            selectable: true,
+            selectHelper: true,
+            select: function (start, end, allDay) {
+                var title = prompt('Event Title:');
+                if (title) {
+                    var start = moment(start, 'DD.MM.YYYY').format('YYYY-MM-DD');
+                    var end = moment(end, 'DD.MM.YYYY').format('YYYY-MM-DD');
+                    $.ajax({
+                        url: "{{ URL::to('createevent') }}",
+                        data: 'title=' + title + '&start=' + start + '&end=' + end +
+                            '&_token=' + "{{ csrf_token() }}",
+                        type: "post",
+                        success: function (data) {
+                            alert("Added Successfully");
+                            $('#calendar').fullCalendar('refetchEvents');
+                        },
+                        error: function (xhr) {
+                            if (xhr.status === 403) {
+                                alert('Only administrators can create events.');
+                            } else {
+                                // Handle other errors
+                                alert('An error occurred. Please try again later.');
+                            }
                         }
-                    }
-                });
-            }
-        },
-        eventColor: '#00491E', // Change event color to blue
-        eventTextColor: 'white' // Change event text color to white
+                    });
+                }
+            },
+            eventClick: function (event) {
+                var deleteMsg = confirm("Do you really want to delete?");
+                if (deleteMsg) {
+                    $.ajax({
+                        type: "POST",
+                        url: "{{ URL::to('deleteevent') }}",
+                        data: "&id=" + event.id + '&_token=' + "{{ csrf_token() }}",
+                        success: function (response) {
+                            if (parseInt(response) > 0) {
+                                $('#calendar').fullCalendar('removeEvents', event.id);
+                                alert("Deleted Successfully");
+                            }
+                        },
+                        error: function (xhr) {
+                            if (xhr.status === 403) {
+                                alert('Only administrators can delete events.');
+                            } else {
+                                // Handle other errors
+                                alert('An error occurred. Please try again later.');
+                            }
+                        }
+                    });
+                }
+            },
+            eventColor: '#00491E', // Change event color to blue
+            eventTextColor: 'white' // Change event text color to white
+        });
     });
-});
 </script>
-
 
 </html>
